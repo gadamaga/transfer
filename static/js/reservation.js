9d6a1074a51900577e97b042d385a89500fe987a
gadamaga <gadamagamyradov08@gmail.com>
2024-10-25 08:02:44 +0500

    25.10. sonky uytgedilen

---
+++ static/js/reservation.js
@@ -0,0 +1,5 @@
+    document.addEventListener('DOMContentLoaded', function() {
+        document.getElementById("reserbtn").addEventListener('click', function() {
+            window.location.href = "reservationII.html";
+        });
+    });
---
+++ static/js/reservationII.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("reservationII").addEventListener('click', function() {
+        window.location.href = "reservationIII.html";
+    });
+});
